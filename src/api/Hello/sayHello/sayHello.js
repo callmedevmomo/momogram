dea6d2bd2923cbc61ca3fb545364e4ed53c68100
momo <callmedevmomo@gmail.com>
2019-05-16 15:05:39 +0900

    Integrating Prisma in our Server

--- src/api/Hello/sayHello/sayHello.js
+++ src/api/Hello/sayHello/sayHello.js
@@ -1,5 +1,9 @@
+import { prisma } from "../../../../generated/prisma-client";
 export default {
   Query: {
-    sayHello: () => "hello"
+    sayHello: async () => {
+      console.log(await prisma.users());
+      return "HELLO";
+    }
   }
 };
