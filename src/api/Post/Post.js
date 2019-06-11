1ebac408838261511053c9afc61766c087874ed6
momo <callmedevmomo@gmail.com>
2019-06-11 17:43:13 +0900

    Kill The Fragments

--- src/api/Post/Post.js
+++ src/api/Post/Post.js
@@ -2,6 +2,9 @@ import { prisma } from "../../../generated/prisma-client";
 
 export default {
   Post: {
+    files: ({ id }) => prisma.post({ id }).files(),
+    comments: ({ id }) => prisma.post({ id }).comments(),
+    user: ({ id }) => prisma.post({ id }).user(),
     isLiked: async (parent, _, { request }) => {
       const { user } = request;
       const { id } = parent;
--- src/api/Post/seeFullPost/seeFullPost.js
+++ src/api/Post/seeFullPost/seeFullPost.js
@@ -1,11 +1,10 @@
 import { prisma } from "../../../../generated/prisma-client";
-import { COMMENT_FRGMENT, FULL_POST_FRAGMENT } from "../../../fragements";
 
 export default {
   Query: {
     seeFullPost: async (_, args) => {
       const { id } = args;
-      return prisma.post({ id }).$fragment(FULL_POST_FRAGMENT);
+      return prisma.post({ id });
     }
   }
 };
