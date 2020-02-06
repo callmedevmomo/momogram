d13de54b869ecdfecae5d9fd2d173b7cc9e97c54
momo <callmedevmomo@gmail.com>
2020-02-06 20:41:55 +0900

    prisma cascade opt

--- fragments.js
+++ fragments.js
@@ -1,9 +1,35 @@
+export const USER_FRAGMENT = `
+    id
+    name
+    
+`;
 export const COMMENT_FRAGEMNET = `
-fragment CommentParts on Comment{
     id
     text
     user{
-        name
+        ${USER_FRAGMENT}
     }    
-}
+
 `;
+
+export const FILE_FRAGEMNET = `
+    id
+    url
+`;
+
+export const FULL_POST_FRAGMENT = `
+fragment PostParts on Post{
+    id
+    location
+    caption
+    files {
+        ${FILE_FRAGEMNET}
+    }
+    comments{
+        ${COMMENT_FRAGEMNET}
+        }
+    
+    user{
+        ${USER_FRAGMENT}
+    }
+}`;
--- src/api/Post/seeFullPost/seeFullPost.js
+++ src/api/Post/seeFullPost/seeFullPost.js
@@ -1,27 +1,11 @@
 import { prisma } from "../../../../generated/prisma-client";
-import { COMMENT_FRAGEMNET } from "../../../../fragments";
+import { FULL_POST_FRAGMENT } from "../../../../fragments";
 
 export default {
   Query: {
-    seeFullPost: async (_, args) => {
+    seeFullPost: (_, args) => {
       const { id } = args;
-      const post = await prisma.post({ id });
-      //comments + fragment
-      const comments = await prisma
-        .post({ id })
-        .comments()
-        .$fragment(COMMENT_FRAGEMNET);
-      const likeCount = await prisma
-        .likesConnection({
-          where: {
-            post: {
-              id
-            }
-          }
-        })
-        .aggregate()
-        .count();
-      return { post, comments, likeCount };
+      return prisma.post({ id }).$fragment(FULL_POST_FRAGMENT);
     }
   }
 };
