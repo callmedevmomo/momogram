1de40d7ed06d3db3356e15d8376b436f7e2b8889
momo <callmedevmomo@gmail.com>
2020-02-06 14:08:27 +0900

    isLiked

--- src/api/Post/Post.js
+++ src/api/Post/Post.js
@@ -5,6 +5,8 @@ export default {
     files: ({ id }) => prisma.post({ id }).files(),
     comments: ({ id }) => prisma.post({ id }).comments(),
     user: ({ id }) => prisma.post({ id }).user(),
+    //Post를 받을 때 마다 parent는 post parent
+    // 요청한 id로 post에 (parent) like가 있는지를 확인
     isLiked: async (parent, _, { request }) => {
       const { user } = request;
       const { id } = parent;
