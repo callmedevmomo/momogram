f7484c28187673127870b7587600a8546688d3fa
momo <callmedevmomo@gmail.com>
2020-02-06 13:58:47 +0900

    myprofile && isFollowing rebuild

--- src/api/User/User.js
+++ src/api/User/User.js
@@ -21,11 +21,14 @@ export default {
     fullName: parent => {
       return `${parent.firstName} ${parent.lastName}`;
     },
+    // parent 상위 resolver return 값 전달 받아 찾는 과정
     isFollowing: async (parent, _, { request }) => {
       const { user } = request;
       const { id: parentId } = parent;
       try {
         return await prisma.$exists.user({
+          //요청한 유저(parentId)가 database에 있는지 확인하고,
+          // 팔로워리스트중(followers_some) 요청한 유저id(user.id)가 있는지
           AND: [{ id: parentId }, { followers_some: { id: user.id } }]
         });
       } catch (error) {
@@ -37,6 +40,7 @@ export default {
       const { user } = request;
       const { id: parentId } = parent;
       return user.id === parentId;
+      // 요청하는 사람(parentId)와 요청한 사람(user.id)이 같으면  myProfile
     }
   }
 };
--- src/api/User/me/me.js
+++ src/api/User/me/me.js
@@ -3,11 +3,20 @@ import { prisma } from "../../../../generated/prisma-client";
 export default {
   Query: {
     me: async (_, __, { request, isAuthenticated }) => {
+      // console.log("user Parent", _);
       isAuthenticated(request);
       const { user } = request;
       const userProfile = await prisma.user({ id: user.id });
       const posts = await prisma.user({ id: user.id }).posts();
       return { user: userProfile, posts };
     }
+    // },
+    // // making Custom resolvers
+    // User: {
+    //   //parent gives me the parent resolvers
+    //   // schema.js에서 모든  resolver를 merge ==> seeUser에서도 fullName 사용 가능
+    //   fullName: parent => {
+    //     return `${parent.firstName} ${parent.lastName}`;
+    //   }
   }
 };
