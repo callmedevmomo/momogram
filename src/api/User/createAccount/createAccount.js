0864353070dd891996300c7ccf98eca8439dd055
momo <callmedevmomo@gmail.com>
2020-02-12 15:17:24 +0900

    User/CreateAccount

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -4,14 +4,14 @@ export default {
   Mutation: {
     createAccount: async (_, args) => {
       const { name, email, firstName = "", lastName = "", bio = "" } = args;
-      const user = await prisma.createUser({
+      await prisma.createUser({
         name,
         email,
         firstName,
         lastName,
         bio
       });
-      return user;
+      return true;
     }
   }
 };
